a30951140f624a278a7e06e78d90eb736c3578e0
srinu2003 <88500103+srinu2003@users.noreply.github.com>
2024-01-02 21:08:11 +0000

    change

--- fifoIPC.c
+++ fifoIPC.c
@@ -4,16 +4,21 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+
 int main() {
   int fd;
   char *myfifo = "/tmp/myfifo";
   mkfifo(myfifo, 0666);
   char arr1[80], arr2[80];
-  while (1) {
+
+  pid_t pid = fork(); // Create a child process
+  if (pid > 0) { // Parent process - writing to FIFO
     fd = open(myfifo, O_WRONLY);
     fgets(arr2, 80, stdin);
     write(fd, arr2, strlen(arr2) + 1);
     close(fd);
+  } 
+  else { // Child process - reading from FIFO
     fd = open(myfifo, O_RDONLY);
     read(fd, arr1, sizeof(arr1));
     printf("User2: %s\n", arr1);
